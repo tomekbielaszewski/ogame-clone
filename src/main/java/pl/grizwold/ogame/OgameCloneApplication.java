71f709d51601b8369e5d00a0ce7513c499f17efb
tbielaszewski <tomekbielaszewski@gmail.com>
2018-07-03 10:16:58 +0200

    Application bootstrap

---
+++ src/main/java/pl/grizwold/ogame/OgameCloneApplication.java
@@ -0,0 +1,12 @@
+package pl.grizwold.ogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgameCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgameCloneApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/grizwold/ogame/OgameCloneApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.grizwold.ogame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OgameCloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
