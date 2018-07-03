3c11148e5536fb9209f816e214fd5519ca35b46a
tbielaszewski <tomekbielaszewski@gmail.com>
2018-07-03 16:05:30 +0200

    Technology tree change when building leveling up - proposal

---
+++ src/main/java/pl/grizwold/ogame/modules/technology/domain/Technology.java
@@ -0,0 +1,4 @@
+package pl.grizwold.ogame.modules.technology.domain;
+
+public class Technology {
+}
---
+++ src/main/java/pl/grizwold/ogame/modules/technology/events/listeners/BuildingLeveledUpListener.java
@@ -0,0 +1,39 @@
+package pl.grizwold.ogame.modules.technology.events.listeners;
+
+import org.springframework.context.event.EventListener;
+import pl.grizwold.ogame.modules.buildings.domain.Building;
+import pl.grizwold.ogame.modules.buildings.events.domain.BuildingLeveledUp;
+import pl.grizwold.ogame.modules.technology.domain.Technology;
+import pl.grizwold.ogame.modules.technology.domain.TechnologyTree;
+
+import java.util.List;
+
+public class BuildingLeveledUpListener {
+
+    @EventListener(BuildingLeveledUp.class)
+    public void execute(BuildingLeveledUp event) {
+        TechnologyTree technologyTree = recalculateTechnologyTree(event.getBuilding());
+        List<Technology> technologies = availableNewTechnology(technologyTree);
+
+        if(!technologies.isEmpty()) {
+            saveTechnologyTree(technologyTree);
+            sendNewTechnologyAvailableEvent(technologies);
+        }
+    }
+
+    private TechnologyTree recalculateTechnologyTree(Building building) {
+        return null;
+    }
+
+    private List<Technology> availableNewTechnology(TechnologyTree technologyTree) {
+        return null;
+    }
+
+    private void saveTechnologyTree(TechnologyTree technologyTree) {
+
+    }
+
+    private void sendNewTechnologyAvailableEvent(List<Technology> technologies) {
+
+    }
+}
