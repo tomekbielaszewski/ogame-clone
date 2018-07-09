350b248ddba4e0fa697339464d063471f7242f14
tbielaszewski <tomekbielaszewski@gmail.com>
2018-07-09 19:13:17 +0200

    building deconstruction - requesting deconstruction

--- src/main/java/pl/grizwold/ogame/modules/buildings/events/listeners/BuildingDestructionRequestedListener.java
+++ src/main/java/pl/grizwold/ogame/modules/buildings/events/listeners/BuildingDestructionRequestedListener.java
@@ -1,46 +1,54 @@
 package pl.grizwold.ogame.modules.buildings.events.listeners;
 
 import org.springframework.context.event.EventListener;
+import pl.grizwold.ogame.modules.buildings.domain.Building;
 import pl.grizwold.ogame.modules.buildings.domain.BuildingType;
-import pl.grizwold.ogame.modules.buildings.events.domain.BuildingDestructionFinished;
+import pl.grizwold.ogame.modules.buildings.domain.ConstructionSite;
+import pl.grizwold.ogame.modules.resources.events.domain.BuildingDeconstructionResourcesLeaseRequested;
 import pl.grizwold.ogame.modules.buildings.events.domain.BuildingDestructionRequested;
-import pl.grizwold.ogame.modules.scheduler.events.domain.ScheduledEventRequested;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import pl.grizwold.ogame.modules.resources.domain.Cost;
 
 public class BuildingDestructionRequestedListener {
 
     @EventListener(BuildingDestructionRequested.class)
-    public ScheduledEventRequested execute(BuildingDestructionRequested event) {
-        String planetId = event.getPlanetId();
+    public BuildingDeconstructionResourcesLeaseRequested execute(BuildingDestructionRequested event) {
         BuildingType buildingType = event.getBuildingType();
+        String planetId = event.getPlanetId();
+        Building building = getBuilding(buildingType, planetId);
 
-        checkBuildingLevel(buildingType, planetId);
-        long destructionDurationInSeconds = calculateDestructionDurationInSeconds(buildingType, planetId);
-        LocalDateTime destructionFinishDateTime = convertDurationToExactDateTime(destructionDurationInSeconds);
-        return scheduledEvent(destructionFinishDateTime, createBuildingDestructionFinished(buildingType, planetId));
+        Cost cost = calculateResourcesNeededToDeconstruct(building);
+        checkBuildingLevel(building);
+        checkResourcesAvailable(cost, planetId);
+        ConstructionSite constructionSite = saveConstructionSite(building);
+
+        return createBuildingDeconstructionResourcesLeaseRequestedEvent(planetId, cost, constructionSite);
     }
 
-    private void checkBuildingLevel(BuildingType buildingType, String planetId) {
+    private Building getBuilding(BuildingType buildingType, String planetId) {
+        // gets building information from modules DB
+        return null;
+    }
+
+    private Cost calculateResourcesNeededToDeconstruct(Building building) {
+        // calculates building deconstruction cost using Ogame formulas
+        return null;
+    }
+
+    private void checkBuildingLevel(Building building) {
         // validate if building level is above 0
-        // if not raise event to the user
     }
 
-    private long calculateDestructionDurationInSeconds(BuildingType buildingType, String planetId) {
-        // calculate destruction time using Ogame formulas
-        return 0;
+    private void checkResourcesAvailable(Cost cost, String planetId) {
+        // validate if there are enough resources on planet
     }
 
-    private LocalDateTime convertDurationToExactDateTime(long destructionDurationInSeconds) {
-        return LocalDateTime.now().plus(destructionDurationInSeconds, ChronoUnit.SECONDS);
+    private ConstructionSite saveConstructionSite(Building building) {
+        // save construction site with target building data (how the building will be after (de)construction)
+        Building targetBuildingState = new Building(building.getLevel() - 1, building.getType(), building.getOwner(), building.getPlanetId());
+        return new ConstructionSite("DB provided ID", targetBuildingState);
     }
 
-    private BuildingDestructionFinished createBuildingDestructionFinished(BuildingType buildingType, String planetId) {
-        return new BuildingDestructionFinished(planetId, buildingType);
-    }
-
-    private ScheduledEventRequested scheduledEvent(LocalDateTime destructionFinishDateTime, BuildingDestructionFinished buildingDestructionFinished) {
-        return new ScheduledEventRequested(destructionFinishDateTime, buildingDestructionFinished);
+    private BuildingDeconstructionResourcesLeaseRequested createBuildingDeconstructionResourcesLeaseRequestedEvent(String planetId, Cost cost, ConstructionSite constructionSite) {
+        return new BuildingDeconstructionResourcesLeaseRequested(cost, constructionSite.getId(), planetId);
     }
 }
