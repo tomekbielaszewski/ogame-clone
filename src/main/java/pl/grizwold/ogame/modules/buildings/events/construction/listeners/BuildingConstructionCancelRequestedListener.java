e6798797f5c01831924cdb1bf614207180ceef58
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-07-12 15:54:53 +0200

    listeners for triggering building (de)construction cancellation process

---
+++ src/main/java/pl/grizwold/ogame/modules/buildings/events/construction/listeners/BuildingConstructionCancelRequestedListener.java
@@ -0,0 +1,36 @@
+package pl.grizwold.ogame.modules.buildings.events.construction.listeners;
+
+import org.springframework.context.event.EventListener;
+import pl.grizwold.ogame.common.domain.Event;
+import pl.grizwold.ogame.modules.buildings.domain.Building;
+import pl.grizwold.ogame.modules.buildings.domain.ConstructionSite;
+import pl.grizwold.ogame.modules.buildings.events.construction.domain.BuildingConstructed;
+import pl.grizwold.ogame.modules.buildings.events.construction.domain.BuildingConstructionCancelRequested;
+import pl.grizwold.ogame.modules.buildings.events.construction.domain.BuildingConstructionFinished;
+import pl.grizwold.ogame.modules.resources.events.domain.ResourcesLeaseReleaseRequested;
+import pl.grizwold.ogame.modules.resources.events.domain.ResourcesLeaseUsed;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static pl.grizwold.ogame.modules.buildings.domain.ConstructionSiteType.CONSTRUCTION;
+
+public class BuildingConstructionCancelRequestedListener {
+
+    @EventListener(BuildingConstructionCancelRequested.class)
+    public ResourcesLeaseReleaseRequested execute(BuildingConstructionCancelRequested event) {
+        ConstructionSite constructionSite = getConstructionSite(event.getCorrelationToken());
+        ResourcesLeaseReleaseRequested nextEvent = null;
+
+        if (constructionSite != null && constructionSite.getType() == CONSTRUCTION) {
+            nextEvent = new ResourcesLeaseReleaseRequested(event);
+        }
+
+        return nextEvent;
+    }
+
+    private ConstructionSite getConstructionSite(String correlationToken) {
+        // get construction site from modules DB
+        return null;
+    }
+}
---
+++ src/main/java/pl/grizwold/ogame/modules/buildings/events/destruction/listeners/BuildingDeconstructionCancelRequestedListener.java
@@ -0,0 +1,33 @@
+package pl.grizwold.ogame.modules.buildings.events.destruction.listeners;
+
+import org.springframework.context.event.EventListener;
+import pl.grizwold.ogame.common.domain.Event;
+import pl.grizwold.ogame.modules.buildings.domain.ConstructionSite;
+import pl.grizwold.ogame.modules.buildings.events.construction.domain.BuildingConstructionCancelRequested;
+import pl.grizwold.ogame.modules.buildings.events.destruction.domain.BuildingDeconstructionCancelRequested;
+import pl.grizwold.ogame.modules.resources.events.domain.ResourcesLeaseReleaseRequested;
+
+import java.util.List;
+
+import static pl.grizwold.ogame.modules.buildings.domain.ConstructionSiteType.CONSTRUCTION;
+import static pl.grizwold.ogame.modules.buildings.domain.ConstructionSiteType.DECONSTRUCTION;
+
+public class BuildingDeconstructionCancelRequestedListener {
+
+    @EventListener(BuildingDeconstructionCancelRequested.class)
+    public ResourcesLeaseReleaseRequested execute(BuildingDeconstructionCancelRequested event) {
+        ConstructionSite constructionSite = getConstructionSite(event.getCorrelationToken());
+        ResourcesLeaseReleaseRequested nextEvent = null;
+
+        if (constructionSite != null && constructionSite.getType() == DECONSTRUCTION) {
+            nextEvent = new ResourcesLeaseReleaseRequested(event);
+        }
+
+        return nextEvent;
+    }
+
+    private ConstructionSite getConstructionSite(String correlationToken) {
+        // get construction site from modules DB
+        return null;
+    }
+}
