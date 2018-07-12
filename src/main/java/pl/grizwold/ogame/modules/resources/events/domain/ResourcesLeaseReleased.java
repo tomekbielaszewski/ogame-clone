f902273590a135160396ef4059db882db90948c3
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-07-12 16:18:23 +0200

    event for releasing resource lease

---
+++ src/main/java/pl/grizwold/ogame/modules/resources/events/domain/ResourcesLeaseReleased.java
@@ -0,0 +1,9 @@
+package pl.grizwold.ogame.modules.resources.events.domain;
+
+import pl.grizwold.ogame.common.domain.Event;
+
+public class ResourcesLeaseReleased extends Event {
+    public ResourcesLeaseReleased(Event event) {
+        super(event);
+    }
+}
