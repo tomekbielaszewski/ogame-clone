bb357a1255efe2cd26cc767243af577dbee5ff68
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-07-12 15:37:21 +0200

    hiding constructors by default to prevent instantiating events from middle of the flow with new correlationtoken value

--- src/main/java/pl/grizwold/ogame/common/domain/Event.java
+++ src/main/java/pl/grizwold/ogame/common/domain/Event.java
@@ -7,12 +7,12 @@ public abstract class Event {
     protected final String ownerId;
     protected final String correlationToken;
 
-    public Event(String ownerId, String correlationToken) {
+    protected Event(String ownerId, String correlationToken) {
         this.ownerId = ownerId;
         this.correlationToken = correlationToken;
     }
 
-    public Event(Event event) {
+    protected Event(Event event) {
         this.ownerId = event.ownerId;
         this.correlationToken = event.correlationToken;
     }
