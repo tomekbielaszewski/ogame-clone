ab0f314fab0041dbee7bd6fea8ceb5e15f98861d
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-07-12 15:55:17 +0200

    event for refunding resources lease

---
+++ src/main/java/pl/grizwold/ogame/modules/resources/events/domain/ResourcesLeaseReleaseRequested.java
@@ -0,0 +1,9 @@
+package pl.grizwold.ogame.modules.resources.events.domain;
+
+import pl.grizwold.ogame.common.domain.Event;
+
+public class ResourcesLeaseReleaseRequested extends Event {
+    public ResourcesLeaseReleaseRequested(Event event) {
+        super(event);
+    }
+}
