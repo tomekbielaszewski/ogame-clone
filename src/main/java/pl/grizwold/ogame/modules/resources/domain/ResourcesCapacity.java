5bb8ade5b2f503e3b5676a45c4160c52cda5d3a0
tbielaszewski <tomekbielaszewski@gmail.com>
2018-07-14 10:13:12 +0200

    handling storage capacity change in resources module

---
+++ src/main/java/pl/grizwold/ogame/modules/resources/domain/ResourcesCapacity.java
@@ -0,0 +1,4 @@
+package pl.grizwold.ogame.modules.resources.domain;
+
+public class ResourcesCapacity {
+}
--- src/main/java/pl/grizwold/ogame/modules/resources/events/listeners/BuildingConstructedListener.java
+++ src/main/java/pl/grizwold/ogame/modules/resources/events/listeners/BuildingConstructedListener.java
@@ -4,14 +4,27 @@ import org.springframework.context.event.EventListener;
 import pl.grizwold.ogame.modules.buildings.domain.Building;
 import pl.grizwold.ogame.modules.buildings.events.construction.domain.BuildingConstructed;
 import pl.grizwold.ogame.modules.resources.domain.ResourceExtractionRate;
+import pl.grizwold.ogame.modules.resources.domain.ResourcesCapacity;
 
 public class BuildingConstructedListener {
 
     @EventListener(BuildingConstructed.class)
     public void execute(BuildingConstructed event) {
-        if(isResourceExtractionUpgraded(event.getBuilding())) {
-            ResourceExtractionRate extractionRate = recalculateResourceExtractionRate(event.getBuilding());
-            saveResourceExtractionRate(extractionRate);
+        handleResourcesExtractionUpgrade(event);
+        handleStorageUpgrade(event);
+    }
+
+    private void handleResourcesExtractionUpgrade(BuildingConstructed event) {
+        if (isResourceExtractionUpgraded(event.getBuilding())) {
+            ResourceExtractionRate extractionRate = recalculateResourcesExtractionRate(event.getBuilding());
+            saveResourcesExtractionRate(extractionRate);
+        }
+    }
+
+    private void handleStorageUpgrade(BuildingConstructed event) {
+        if (isResourcesStorageUpgraded(event.getBuilding())) {
+            ResourcesCapacity resourcesCapacity = recalculateResourcesCapacity(event.getBuilding());
+            saveResourcesCapacity(resourcesCapacity);
         }
     }
 
@@ -19,11 +32,23 @@ public class BuildingConstructedListener {
         return false;
     }
 
-    private ResourceExtractionRate recalculateResourceExtractionRate(Building building) {
+    private ResourceExtractionRate recalculateResourcesExtractionRate(Building building) {
         return null;
     }
 
-    private void saveResourceExtractionRate(ResourceExtractionRate extractionRate) {
+    private void saveResourcesExtractionRate(ResourceExtractionRate extractionRate) {
+
+    }
+
+    private boolean isResourcesStorageUpgraded(Building building) {
+        return false;
+    }
+
+    private ResourcesCapacity recalculateResourcesCapacity(Building building) {
+        return null;
+    }
+
+    private void saveResourcesCapacity(ResourcesCapacity resourcesCapacity) {
 
     }
 }
