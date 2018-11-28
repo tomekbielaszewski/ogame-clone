db8213fdcaefc6e2fd19a879ee21c0e4186c40a2
Grizwold <tomekbielaszewski@gmail.com>
2018-11-28 23:10:40 +0100

    Building construction start implemented

--- src/main/java/pl/grizwold/ogame/modules/buildings/events/construction/listeners/BuildingConstructionRequestedListener.java
+++ src/main/java/pl/grizwold/ogame/modules/buildings/events/construction/listeners/BuildingConstructionRequestedListener.java
@@ -1,16 +1,34 @@
 package pl.grizwold.ogame.modules.buildings.events.construction.listeners;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import pl.grizwold.ogame.common.domain.Event;
-import pl.grizwold.ogame.modules.buildings.domain.Building;
-import pl.grizwold.ogame.modules.buildings.domain.BuildingType;
-import pl.grizwold.ogame.modules.buildings.domain.ConstructionSite;
-import pl.grizwold.ogame.modules.buildings.domain.ConstructionSiteType;
+import pl.grizwold.ogame.modules.buildings.dao.BuildingDao;
+import pl.grizwold.ogame.modules.buildings.dao.ConstructionSiteDao;
+import pl.grizwold.ogame.modules.buildings.dao.InitialBuildingCostDao;
+import pl.grizwold.ogame.modules.buildings.domain.*;
 import pl.grizwold.ogame.modules.buildings.events.construction.domain.BuildingConstructionRequested;
+import pl.grizwold.ogame.modules.resources.client.ResourcesEventFactory;
+import pl.grizwold.ogame.modules.resources.client.ResourcesReadService;
 import pl.grizwold.ogame.modules.resources.domain.Cost;
 import pl.grizwold.ogame.modules.resources.events.domain.ResourcesLeaseRequested;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class BuildingConstructionRequestedListener {
+    private BuildingDao buildingDao;
+    private ConstructionSiteDao constructionSiteDao;
+    private InitialBuildingCostDao initialBuildingCostDao;
+    private ResourcesReadService resourcesService;
+
+    @Autowired
+    public BuildingConstructionRequestedListener(BuildingDao buildingDao, ConstructionSiteDao constructionSiteDao, InitialBuildingCostDao initialBuildingCostDao, ResourcesReadService resourcesService) {
+        this.buildingDao = buildingDao;
+        this.constructionSiteDao = constructionSiteDao;
+        this.initialBuildingCostDao = initialBuildingCostDao;
+        this.resourcesService = resourcesService;
+    }
 
     @EventListener(BuildingConstructionRequested.class)
     public ResourcesLeaseRequested execute(BuildingConstructionRequested event) {
@@ -21,34 +39,44 @@ public class BuildingConstructionRequestedListener {
         checkResourcesAvailable(cost, planetId);
         createConstructionSite(event.getCorrelationToken(), building);
 
-        return createResourcesLeaseRequestEvent(event, cost, planetId);
+        return createResourcesLease(event, cost, planetId);
     }
 
     private Building getBuilding(BuildingType buildingType, String planetId) {
-        // get building from modules database
-        // or create new with 0 lvl
-        return new Building(1, buildingType, "ownerId", planetId);
+        return buildingDao.findByTypeAndPlanetId(buildingType, planetId)
+                .orElse(new Building(BigInteger.ZERO, buildingType, "ownerId", planetId));
     }
 
     private Cost calculateResourcesNeededToLevelUp(Building building) {
-        // get actual building level
-        // using Ogame formula calculate the cost for level up
-        return new Cost(48, 24, 0);
+        InitialBuildingCost initialCost = initialBuildingCostDao.getByBuildingType(building.getType());
+        BigInteger targetLevel = building.getLevel().add(BigInteger.ONE);
+        return new Cost(
+                calculateResource(targetLevel, initialCost.getInitialCost().getMetal(), initialCost.getMultiplier()),
+                calculateResource(targetLevel, initialCost.getInitialCost().getCrystal(), initialCost.getMultiplier()),
+                calculateResource(targetLevel, initialCost.getInitialCost().getDeuterium(), initialCost.getMultiplier())
+        );
+    }
+
+    private int calculateResource(BigInteger level, int resource, BigDecimal multiplier) {
+        return multiplier.pow(level.subtract(BigInteger.ONE).intValue())
+                .multiply(BigDecimal.valueOf(resource))
+                .intValue();
     }
 
     private void checkResourcesAvailable(Cost cost, String planetId) {
-        // use resource module to check if resource cost is spendable
-        // throws InsufficientResourcesException?
-        // rises InsufficientResources event?
+        if (!resourcesService.hasEnoughResources(cost, planetId)) {
+            // throws InsufficientResourcesException?
+            // rises InsufficientResources event?
+        }
     }
 
     private void createConstructionSite(String correlationToken, Building building) {
-        // save construction site with building data
-        Building targetBuildingState = new Building(building.getLevel() + 1, building.getType(), building.getOwner(), building.getPlanetId());
-        new ConstructionSite(correlationToken, targetBuildingState, ConstructionSiteType.CONSTRUCTION);
+        BigInteger targetLevel = building.getLevel().add(BigInteger.ONE);
+        ConstructionSite constructionSite = new ConstructionSite(correlationToken, building.getType(), building.getPlanetId(), targetLevel, ConstructionSiteType.CONSTRUCTION);
+        constructionSiteDao.save(constructionSite);
     }
 
-    private ResourcesLeaseRequested createResourcesLeaseRequestEvent(Event source, Cost cost, String planetId) {
-        return new ResourcesLeaseRequested(source, cost, planetId);
+    private ResourcesLeaseRequested createResourcesLease(Event source, Cost cost, String planetId) {
+        return ResourcesEventFactory.createResourcesLeaseRequestedEvent(source, cost, planetId);
     }
 }
